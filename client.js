cf2e7d5858645b488720602319d90572e58e142a
unknown <timamdad@hawk.iit.edu>
2019-03-31 02:53:32 -0500

    initial commit

---
+++ client.js
@@ -0,0 +1,46 @@
+import alt from 'alt';
+
+let view = new alt.WebView("http://resources/speedo/html/index.html");
+
+let localPlayer = alt.getLocalPlayer();
+let speedoShown = false;
+
+
+
+
+alt.on('update', () => {
+    if(!localPlayer){
+        localPlayer = alt.getLocalPlayer();
+    }
+    let veh = localPlayer.vehicle;
+    if (veh !== null && speedoShown){
+        view.emit('drawSpeedo', Math.ceil(veh.speed*2.236936), veh.gear, veh.rpm)
+    }
+
+  });
+
+
+view.on('speedoLoaded', () => {
+    speedoShown = true;
+});
+
+view.on('speedoUnloaded', () => {
+    speedoShown = false;
+})
+
+
+alt.onServer("playerEnterVehicle", (seat) => {
+    if (seat == 1){ //driver
+        if (!speedoShown){
+          view.emit('showSpeedo', true);
+        }
+    }
+})
+
+alt.onServer("playerLeftVehicle", (seat) => {
+    if (seat == 1){ //driver
+        if (speedoShown){
+          view.emit('showSpeedo', false);
+        }
+    }
+})
---
+++ index.mjs
@@ -0,0 +1,10 @@
+import alt from 'alt';
+
+alt.on('playerEnteredVehicle', ( player, vehicle, seat) => {
+  console.log(`${player.name} enteredd ${vehicle.id} seat ${seat}`);
+  alt.emitClient(player, 'playerEnterVehicle', seat);
+});
+
+alt.on('playerLeftVehicle', ( player, vehicle, seat) => {
+  alt.emitClient(player, 'playerLeftVehicle', seat);
+});
